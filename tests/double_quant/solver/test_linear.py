42a4aa7d200c6db5832d4d9cd0b32d330ff51c24
11D-Beyonder <ztz20001117@gmail.com>
2026-01-15 16:34:45 +0800

    test(linear): qpe qubit order

--- tests/double_quant/solver/test_linear.py
+++ tests/double_quant/solver/test_linear.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import flag
 import numpy as np
 import pytest
 from double_quant.solver.linear import (
@@ -5,7 +6,61 @@ from double_quant.solver.linear import (
     QuantumLinearSolver,
     SciPyLinearSolver,
 )
-import matplotlib.pyplot as plt
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit.library import (
+    ExactReciprocalGate,
+    StatePreparation,
+    UCRYGate,
+    phase_estimation,
+)
+from qiskit.quantum_info import Statevector
+
+
+class TestHhlQubitOrder:
+    """Test bit ordering differences between old and new phase_estimation implementations."""
+
+    def test_qpe_order(self):
+        """
+        Compare the output bit order of deprecated PhaseEstimation vs new phase_estimation.
+
+        Tests with a 4x4 unitary matrix. The key finding is that BOTH implementations
+        now output the same bit order due to how reverse_bits() works.
+        """
+        # Create a 4x4 unitary matrix (2 qubits)
+        # Use a simple diagonal matrix with known eigenvalues
+        # U = diag(e^(2πi*0.25), e^(2πi*0.5), e^(2πi*0.75), e^(2πi*0.0))
+        phases = np.array([-0.25, -0.5, -0.75, 0.0])
+        U = np.diag(np.exp(2j * np.pi * phases))
+
+        vector_reg = QuantumRegister(2, "vector")
+        phase_reg = QuantumRegister(3, "phase")
+        flag_reg = QuantumRegister(1, "flag")
+        # Create the unitary circuit
+        unitary_circuit = QuantumCircuit(vector_reg.size, name="U")
+        unitary_circuit.unitary(U, unitary_circuit.qubits)
+
+        circuit = QuantumCircuit(vector_reg, phase_reg, flag_reg)
+
+        circuit.append(StatePreparation([0, 0, 1, 0]), vector_reg[:])
+        circuit.append(
+            phase_estimation(phase_reg.size, unitary_circuit),
+            phase_reg[:] + vector_reg[:],
+        )
+        print("")
+        print(Statevector.from_circuit(circuit).probabilities_dict([2]))
+        print(Statevector.from_circuit(circuit).probabilities_dict([3]))
+        print(Statevector.from_circuit(circuit).probabilities_dict([4]))
+        print(Statevector.from_circuit(circuit).probabilities_dict([2, 3, 4]))
+        circuit.append(
+            ExactReciprocalGate(phase_reg.size, 0.25 / 2**phase_reg.size, True),
+            phase_reg[::-1] + flag_reg[:],
+        )
+        # circuit.append(
+        #     UCRYGate([0, 0, 0, 0, 2 * np.arcsin(1), 0, 0, 0]),
+        #     flag_reg[:] + phase_reg[::-1],
+        # )
+        print(f"\n{'=' * 60}\n{circuit.decompose('1/x').draw()}")
+        print(Statevector.from_circuit(circuit).probabilities_dict([5]))
 
 
 class TestQuantumLinearSolver:
@@ -14,8 +69,9 @@ class TestQuantumLinearSolver:
         num_ele = 2**2
         solver._linear_system = QuantumLinearSolver._LinearSystem.random(num_ele)
         circuit = solver._construct_circuit()
-        circuit.decompose().draw()
-        plt.show()
+        print(f"\n{circuit.decompose('circuit-43').decompose('1/x').draw()}")
+        # circuit.draw("mpl")
+        # plt.show()
 
 
 class TestNumPyLinearSolver:
